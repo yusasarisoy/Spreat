8090f790f49032456d28545962b95ab1a07b0aa8
Yuşa Sarısoy <yusasarisoy@icloud.com>
2020-10-09 19:17:22 +0300

    Initial Commit

---
+++ Spreat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spreat
+//
+//  Created by Yuşa Sarısoy on 9.10.2020.
+//  Copyright © 2020 Yuşa Sarısoy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
