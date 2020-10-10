0a6070b57cb64c1969e2599d6ee2ba52137c22a3
Yuşa Sarısoy <yusasarisoy@icloud.com>
2020-10-10 14:34:25 +0300

    Extensions have been added to format and convert Int to String.

---
+++ Spreat/Extensions/Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Extension.swift
+//  Spreat
+//
+//  Created by Yuşa Sarısoy on 9.10.2020.
+//  Copyright © 2020 Yuşa Sarısoy. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    func formatNumber() -> String {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .decimal
+        return formatter.string(from: NSNumber(value: self))!
+    }
+}
+
+extension Int64 {
+    func formatNumber() -> String {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .decimal
+        return formatter.string(from: NSNumber(value: self))!
+    }
+}
