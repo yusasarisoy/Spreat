76a0b2f08cd9b5be06451b3972be223468edba12
Yuşa Sarısoy <yusasarisoy@icloud.com>
2020-10-29 00:25:46 +0300

    The name of an extension file has been updated.

--- Spreat/Extensions/Int+Extensions.swift
+++ Spreat/Extensions/Int+Extensions.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 extension Int {
+    
     func formatNumber() -> String {
         let formatter = NumberFormatter()
         formatter.groupingSeparator = ","
         formatter.numberStyle = .decimal
         return formatter.string(from: NSNumber(value: self))!
     }
+    
 }
